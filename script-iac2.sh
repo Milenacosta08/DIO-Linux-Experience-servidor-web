6a721e1d6e1a1ad198d0d394d00162eb707bc93e
Milenacosta08 <milena.gondim.costa08@aluno.ifce.edu.br>
2022-08-27 23:12:42 -0300

    first commit

---
+++ script-iac2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Atualizando sistema..."
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando apache2..."
+apt-get install apache2 -y
+
+echo "Instalando unzip..."
+apt-get install unzip -y
+
+echo "Baixando e copiando os arquivos da apicação..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-projeto2-iac/archive/refs/heads/main.zip
+
+unzip main.zip
+
+cd linux-projeto2-iac-main
+
+cp -R * /var/www/html/
